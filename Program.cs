9589edb2f6af92d6b2be4190aa4d3b729789d83e
kzlager <highcolour@yandex.ru>
2022-02-17 09:03:09 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 30: Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Дан массив из чисел:");
+double[] array = { 342, 237, 32, 41, 54, 611, 78, 83, 94 };
+double count = array.Length;
+for (int i = 0; i < count; i++)
+    Console.Write($" {array[i]}");
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+        Console.WriteLine($"Куб числа {array[i]}: {array[i]} * {array[i]} * {array[i]} = {array[i] * array[i] * array[i]}");
+}
